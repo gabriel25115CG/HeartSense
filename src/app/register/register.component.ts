7973ac2c623db26d0dec279e86c8f6dea4dd9c62
Gabriel Chalmet <gabri@PC-Gabriel.labo.priv>
2024-12-19 17:10:12 +0100

    Add Login/register form + add navbar redirect

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RegisterformComponent } from "../../components/registerform/registerform.component";
 import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-register',
-  imports: [RegisterformComponent, NavbarComponent],
+  imports: [RegisterformComponent, NavbarComponent, FooterComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
