ade8bb40c1ef7f23f0a28dbfaf595cb4e26f11dd
Gabriel Chalmet <gabri@PC-Gabriel.labo.priv>
2024-12-20 15:25:56 +0100

    Add citation

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,12 +5,12 @@ import { TitleComponent } from '../../components/title/title.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { SeparatorComponent } from '../../components/separator/separator.component';
 import { FeaturesComponent } from '../../components/features/features.component';
-
+import { CitationComponent } from "../../components/citation/citation.component";
 
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent, CardsComponent, TitleComponent, FooterComponent, SeparatorComponent,FeaturesComponent ],
+  imports: [NavbarComponent, CardsComponent, TitleComponent, FooterComponent, SeparatorComponent, FeaturesComponent, CitationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
